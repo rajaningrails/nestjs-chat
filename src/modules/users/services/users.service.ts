ef7f0ddd58db9a992e9fcbd45f5a0d2172efa3c8
Rajan Raj Shah <rajanraj.ingrails@gmail.com>
2026-01-21 23:11:07 +0545

    fix: get users data

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -11,9 +11,27 @@ export class UsersService {
 
   async findUserById(id: number) {
     const [user] = await this.vedaDb.query(
-      'SELECT * FROM user WHERE id = ?',
+      `
+    SELECT
+      u.id,
+      u.level,
+      COALESCE(rh.logo, sd.image, st.image) AS image,
+      COALESCE(sd.name, u.name) AS name
+    FROM users u
+    LEFT JOIN res_home rh
+      ON u.level = 'client'
+     AND rh.user_id = u.id
+    LEFT JOIN student_detail sd
+      ON u.level IN ('student', 'parent')
+     AND sd.user_id = u.id
+    LEFT JOIN staff st
+      ON u.level NOT IN ('student', 'parent', 'client')
+     AND st.user_id = u.id
+    WHERE u.id = ?
+    `,
       [id],
     );
+
     return user ?? null;
   }
 }
